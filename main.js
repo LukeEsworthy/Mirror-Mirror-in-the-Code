88934122f2b20167ac98c3d42f7fc34fb36b66d7
Luke Esworthy <lukesworthy@gmail.com>
2020-04-24 10:59:03 -0500

    worked through with instructor

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let inputBox = document.getElementById("message");
-let outputBox = document.getElementById("articleTwo");
-function displayInputMessage(event) {
-  console.log(event);
-  outputBox.textContent = event.target.value;
-}
-inputBox.addEventListener("keyup", displayInputMessage);
+// Tested code before going through the exercise with instructor:
+// let inputBox = document.getElementById("message");
+// let outputBox = document.querySelectorAll(".article-box");
+// function displayInputMessage(event) {
+//   console.log(event);
+//   outputBox.textContent = event.target.value;
+// }
+// inputBox.addEventListener("keyup", displayInputMessage);
+
+document.querySelector("#message").addEventListener("keyup", function (event) {
+  let boxes = document.querySelectorAll(".article-box");
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].innerHTML = event.target.value;
+  }
+});
